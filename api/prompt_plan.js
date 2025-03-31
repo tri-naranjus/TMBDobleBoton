aa792d7c659776c0681e4316bd2fbe87fd5e2af2
tri-naranjus <36755608+tri-naranjus@users.noreply.github.com>
2025-03-31 09:26:12 +0200

    Update prompt_plan.js

--- api/prompt_plan.js
+++ api/prompt_plan.js
@@ -2,6 +2,7 @@
 const prompt = `
 Diseña plan nutricional diario (3 comidas principales + pre/post entreno)
 Se te pasan todo los datos del usuario utiliza el GET para calcular el menu diario
+el resultado que sea en formato claro para ver en una web y que incluya los horarios de las comidas
 utiliza la info que tienes en memoria
 
 `;
